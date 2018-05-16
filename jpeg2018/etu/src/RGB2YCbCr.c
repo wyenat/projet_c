935747cf5aefc90dc6b1b467a1bf7a642802b54a
Antoine LEVOTRE <antoine.levotre@gmail.com>
2018-05-16 17:18:17 +0200

    RGB2YCbCr : Prise en charge du cas ou l'image n'est pas en couleur. La fonction a maintenant besoin d'un booléen "couleur" pour lui dire si le bitstream correspond a une image en RGB ou en niveaux de gris.

--- jpeg2018/etu/src/RGB2YCbCr.c
+++ jpeg2018/etu/src/RGB2YCbCr.c
@@ -13,20 +13,25 @@ struct YCbCr    {
 };
 
 
-void main(char *bitstream[])  {
+void main(char *bitstream[], bool couleur)  {
 
-    struct YCbCr vect_YCbCr;
+    if (couleur) {
+        struct YCbCr vect_YCbCr;
 
-    vect_YCbCr = malloc(sizeof(bitstream));
+        vect_YCbCr = malloc(sizeof(bitstream));
 
-    length = sizeof(bitstream)/ sizeof(bitstream[0]);
-
-    for (int i=0; i <length; i++)   {
-        vect_YCbCr.Y[i] = 0.299*bistream[3*i] + 0.587*bitstream[3*i+1] + 0.114*bitstream[3*i+2];
-        vect_YCbCr.Cb[i] = -0.1687*bistream[3*i] -0.3313*bitstream[3*i+1] + 0.5*bitstream[3*i+2] + 128;
-        vect_YCbCr.Cr[i] = 0.5*bistream[3*i] - 0.4187*bitstream[3*i+1] - 0.0813*bitstream[3*i+2] + 128;
+        length = sizeof(bitstream) / sizeof(bitstream[0]);
+        for (int i = 0; i < length; i++) {
+            vect_YCbCr.Y[i] = (float) (0.299 * bistream[3 * i] + 0.587 * bitstream[3 * i + 1] +
+                                       0.114 * bitstream[3 * i + 2]);
+            vect_YCbCr.Cb[i] = (float) (-0.1687 * bistream[3 * i] - 0.3313 * bitstream[3 * i + 1] +
+                                        0.5 * bitstream[3 * i + 2] + 128);
+            vect_YCbCr.Cr[i] = (float) (0.5 * bistream[3 * i] - 0.4187 * bitstream[3 * i + 1] -
+                                        0.0813 * bitstream[3 * i + 2] + 128);
+        }
+        free(bitstream);
+        bitstream = vect_YCbCr;
     }
-    free(bitstream);
-    bitstream = vect_YCbCr;
+    // Si ce n'est pas en couleur, on laisse bitstream tel quel.
 
 }
