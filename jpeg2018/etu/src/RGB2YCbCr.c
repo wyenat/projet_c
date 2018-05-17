ba74309a44a74d000e0b632e3bbe3a3276d09290
Antoine LEVOTRE <antoine.levotre@gmail.com>
2018-05-17 15:21:50 +0200

    RGB2YCbCr : Version fonctionelle. Ajout du fichier .h associé

--- jpeg2018/etu/src/RGB2YCbCr.c
+++ jpeg2018/etu/src/RGB2YCbCr.c
@@ -4,56 +4,45 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <errno.h>
+#include <assert.h>
 
 
+float *RGB2YCbCr_couleur(char *bitstream[], size_t length)  {
+    // length est égal à 3 fois le nombre de pixel (3 octets pour le RGB)
+    assert(length%3 == 0);
+    float *vect_YCbCr = malloc(sizeof(float) * length);
 
-
-float *RGB2YCbCr_couleur(char *bitstream)  {
-
-    float *vect_YCbCr = malloc(sizeof(bitstream));
-
-    size_t length = sizeof(bitstream) / sizeof(bitstream[0]);
-
-
-    for (size_t i = 0; i < length; i++) {
-        vect_YCbCr[3*i] = (float) (0.299 * (double)strtol(&bitstream[3 * i], NULL, 16) + 0.587 * (double)strtol(&bitstream[3 * i+1], NULL, 16) +
-                                   0.114 * (double)strtol(&bitstream[3 * i+2], NULL, 16));
-        vect_YCbCr[3*i+1] = (float) (-0.1687 * (double)strtol(&bitstream[3 * i], NULL, 16) - 0.3313 * (double)strtol(&bitstream[3 * i+1], NULL, 16) +
-                                    0.5 * (double)strtol(&bitstream[3 * i+2], NULL, 16) + 128);
-        vect_YCbCr[3*i+2] = (float) (0.5 * (double)strtol(&bitstream[3 * i], NULL, 16) - 0.4187 * (double)strtol(&bitstream[3 * i+1], NULL, 16) -
-                                    0.0813 * (double)strtol(&bitstream[3 * i+2], NULL, 16) + 128);
+    for (size_t i = 0; i < length/3; i++) {
+        vect_YCbCr[3*i] = (float) (0.299 * (double)strtol(bitstream[3 * i], NULL, 16) + 0.587 * (double)strtol(bitstream[3 * i+1], NULL, 16) +
+                                   0.114 * (double)strtol(bitstream[3 * i+2], NULL, 16));
+        printf("Valeur ajoutée dans le nouveau vecteur en Y : %f\n", vect_YCbCr[3*i]);
+        vect_YCbCr[3*i+1] = (float) (-0.1687 * (double)strtol(bitstream[3 * i], NULL, 16) - 0.3313 * (double)strtol(bitstream[3 * i+1], NULL, 16) +
+                                    0.5 * (double)strtol(bitstream[3 * i+2], NULL, 16) + 128);
+        vect_YCbCr[3*i+2] = (float) (0.5 * (double)strtol(bitstream[3 * i], NULL, 16) - 0.4187 * (double)strtol(bitstream[3 * i+1], NULL, 16) -
+                                    0.0813 * (double)strtol(bitstream[3 * i+2], NULL, 16) + 128);
     }
-    free(bitstream);
     return vect_YCbCr;
-    // Si ce n'est pas en couleur, on laisse bitstream tel quel.
 }
 
 
-float *RGB2YCbCr_gris(char *bitstream)  {
-
-    float *vect_YCbCr = malloc(sizeof(bitstream));
-
-    size_t length = sizeof(bitstream) / sizeof(bitstream[0]);
-
+float *RGB2YCbCr_gris(char* bitstream[], size_t length)  {
+    printf("Length : %zu\n", length);
+    float *vect_YCbCr = malloc(sizeof(float) * length);
 
     for (size_t i = 0; i < length; i++) {
-        vect_YCbCr[i] = (float) bitstream[i];
+        vect_YCbCr[i] = (float) strtol(bitstream[i], NULL, 16);
+        printf("Valeur ajoutée dans le nouveau vecteur : %f\n", vect_YCbCr[i]);
     }
-    free(bitstream);
     return vect_YCbCr;
 }
 
 
 int main(int argc, char *argv[])    {
-    char bitstream[5] = {'ff', '0f', '00', 'ff', '00'};
-    if (couleur)    {
-        RGB2YCbCr_couleur(bitstream);
-    }
-    else    {
-        RGB2YCbCr_gris(bitstream)
-    }
-
+    char *bitstream_gris[5] = {"ff", "0f", "00", "ff", "00"};
+    float *new_vect_gris = RGB2YCbCr_gris(bitstream_gris, (size_t) 5);
+    free(new_vect_gris);
+    char *bitstream_couleurs[6] = {"ff", "0f", "00", "15", "a3", "4d"};
+    float *new_vect_couleurs = RGB2YCbCr_couleur(bitstream_couleurs, (size_t) 6);
+    free(new_vect_couleurs);
     return 0;
 }
