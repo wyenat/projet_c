0c45f27090fec9d536181f34b1e83e8cfc8a0f76
Antonin Poché <antonin.poche@grenoble-inp.org>
2018-05-17 15:08:42 +0200

    Quantification: Done bitches

--- jpeg2018/etu/src/quantification.c
+++ jpeg2018/etu/src/quantification.c
@@ -19,32 +19,32 @@ int16_t *quantifier(int16_t *table, int booleen_tableY)
     int16_t *table_quantifiee = malloc(64*sizeof(int16_t));;
     for (int indice = 0; indice < 64; indice++) {
         float ratio = table[indice]/table_quantification[indice];
-        table_quantifiee[indice] = floor(ratio+0.5);
+        table_quantifiee[indice] = (int16_t)(ratio+0.5)/1;
     }
     return table_quantifiee;
 }
 
-void afficher(int16_t *vecteur)
-{
-    printf("\n");
-    for (int indice = 0; indice < 64; indice++) {
-        printf("%d, ", vecteur[indice]);
-        if ((indice+1)%8 == 0) {
-            printf("\n");
-        }
-    }
-    printf("\n");
-}
-
-// parce qu'il faut bien tester
-int main(void)
-{
-    int16_t vecteur_test[64];
-    for (int indice = 0; indice < 64; indice++) {
-        vecteur_test[indice] = pow(indice, 2);
-    }
-    afficher(vecteur_test);
-    int16_t *vecteur_quantifie = quantifier(vecteur_test, 1);
-    afficher(vecteur_quantifie);
-    return 0;
-}
+// void afficher(int16_t *vecteur)
+// {
+//     printf("\n");
+//     for (int indice = 0; indice < 64; indice++) {
+//         printf("%hx\t", vecteur[indice]);
+//         if ((indice+1)%8 == 0) {
+//             printf("\n");
+//         }
+//     }
+//     printf("\n");
+// }
+//
+// // parce qu'il faut bien tester
+// int main(void)
+// {
+//     int16_t vecteur_test[64];
+//     for (int indice = 0; indice < 64; indice++) {
+//         vecteur_test[indice] = (indice-32);
+//     }
+//     afficher(vecteur_test);
+//     int16_t *vecteur_quantifie = quantifier(vecteur_test, 0);
+//     afficher(vecteur_quantifie);
+//     return 0;
+// }
