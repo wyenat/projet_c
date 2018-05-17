62522ef0b8812f2db7ee1a981d09538bd58cda60
Antoine LEVOTRE <antoine.levotre@gmail.com>
2018-05-17 14:24:09 +0200

    RGB2YCbCr : Version intermédiaire, sepération en deux programmes, un pour les couleurs, un pour le gris. (Contient des erreurs !!)

--- jpeg2018/etu/src/RGB2YCbCr.c
+++ jpeg2018/etu/src/RGB2YCbCr.c
@@ -4,38 +4,56 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <errno.h>
 
-struct YCbCr    {
-    float *Y[];
-    float *Cb[];
-    float *Cr[];
-};
 
 
-void main(char *bitstream[], bool couleur)  {
 
-    if (couleur) {
-        struct YCbCr vect_YCbCr;
-        length = sizeof(bitstream) / sizeof(bitstream[0]);
+float *RGB2YCbCr_couleur(char *bitstream)  {
 
-        vect_YCbCr.Y = malloc(length * sizeof(float));
-        vect_YCbCr.Cb = malloc(length * sizeof(float));
-        vect_YCbCr.Cr = malloc(length * sizeof(float));
+    float *vect_YCbCr = malloc(sizeof(bitstream));
+
+    size_t length = sizeof(bitstream) / sizeof(bitstream[0]);
 
 
-        length = sizeof(bitstream) / sizeof(bitstream[0]);
-        for (int i = 0; i < length; i++) {
-            vect_YCbCr.Y[i] = (float) (0.299 * bistream[3 * i] + 0.587 * bitstream[3 * i + 1] +
-                                       0.114 * bitstream[3 * i + 2]);
-            vect_YCbCr.Cb[i] = (float) (-0.1687 * bistream[3 * i] - 0.3313 * bitstream[3 * i + 1] +
-                                        0.5 * bitstream[3 * i + 2] + 128);
-            vect_YCbCr.Cr[i] = (float) (0.5 * bistream[3 * i] - 0.4187 * bitstream[3 * i + 1] -
-                                        0.0813 * bitstream[3 * i + 2] + 128);
-        }
-        free(bitstream);
-        bitstream = vect_YCbCr;
+    for (size_t i = 0; i < length; i++) {
+        vect_YCbCr[3*i] = (float) (0.299 * (double)strtol(&bitstream[3 * i], NULL, 16) + 0.587 * (double)strtol(&bitstream[3 * i+1], NULL, 16) +
+                                   0.114 * (double)strtol(&bitstream[3 * i+2], NULL, 16));
+        vect_YCbCr[3*i+1] = (float) (-0.1687 * (double)strtol(&bitstream[3 * i], NULL, 16) - 0.3313 * (double)strtol(&bitstream[3 * i+1], NULL, 16) +
+                                    0.5 * (double)strtol(&bitstream[3 * i+2], NULL, 16) + 128);
+        vect_YCbCr[3*i+2] = (float) (0.5 * (double)strtol(&bitstream[3 * i], NULL, 16) - 0.4187 * (double)strtol(&bitstream[3 * i+1], NULL, 16) -
+                                    0.0813 * (double)strtol(&bitstream[3 * i+2], NULL, 16) + 128);
     }
+    free(bitstream);
+    return vect_YCbCr;
     // Si ce n'est pas en couleur, on laisse bitstream tel quel.
+}
 
+
+float *RGB2YCbCr_gris(char *bitstream)  {
+
+    float *vect_YCbCr = malloc(sizeof(bitstream));
+
+    size_t length = sizeof(bitstream) / sizeof(bitstream[0]);
+
+
+    for (size_t i = 0; i < length; i++) {
+        vect_YCbCr[i] = (float) bitstream[i];
+    }
+    free(bitstream);
+    return vect_YCbCr;
+}
+
+
+int main(int argc, char *argv[])    {
+    char bitstream[5] = {'ff', '0f', '00', 'ff', '00'};
+    if (couleur)    {
+        RGB2YCbCr_couleur(bitstream);
+    }
+    else    {
+        RGB2YCbCr_gris(bitstream)
+    }
+
+    return 0;
 }
