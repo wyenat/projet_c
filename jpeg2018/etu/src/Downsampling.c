7f35d6c37c9d623811917cc426c2becd6ef5b3f1
Antoine LEVOTRE <antoine.levotre@gmail.com>
2018-05-20 18:08:50 +0200

    Downsampling : Premier jet de la fonction. Compresse les images couleurs avec des MCU valables (2x1, 1x2 ou 2x2).

---
+++ jpeg2018/etu/src/Downsampling.c
@@ -0,0 +1,67 @@
+//
+// Created by antoine on 20/05/18.
+//
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <assert.h>
+#include "DCT.h"
+
+struct MCU_16 * downsampling_horizontal_2_blocs(struct MCU_16 * MCU_entree)   {
+    // Compresse un MCU 2x? en un MCU 1x?. ATTENTION !! Ne marche que sur des MCU de largeur 2 !
+    int16_t *flux_sortie = malloc(MCU_entree->hauteur * 64 * sizeof(int16_t));
+    for (int8_t bloc_vertical = 0; bloc_vertical<MCU_entree->hauteur; bloc_vertical++) {
+        for (int i = 0; i < 64; i++) {
+            flux_sortie[64*bloc_vertical + i] = (MCU_entree->flux[2 * (64*bloc_vertical + i)] + MCU_entree->flux[2 * (64*bloc_vertical + i) + 1]) / (int16_t) 2;
+        }
+    }
+    struct MCU_16 *MCU_sortie = malloc(sizeof(struct MCU_16));
+    MCU_sortie->hauteur = MCU_entree->hauteur;
+    MCU_sortie->largeur = MCU_sortie->largeur / (uint8_t) 2;    // Sera en pratique toujours égal à 1.
+    MCU_sortie->couleur = MCU_entree->couleur;          // Idem, en pratique toujours égal à 1, vu qu'on ne compresse que les images couleurs.
+    MCU_sortie->echant_l = 1;
+    MCU_sortie->echant_h = MCU_entree->echant_h;
+    MCU_sortie->flux = flux_sortie;
+
+    free(MCU_entree);
+    return MCU_sortie;
+}
+
+
+struct MCU_16 * downsampling_vertical_2_blocs(struct MCU_16 * MCU_entree)   {
+    // Compresse un MCU ?x2 en un MCU ?x1. ATTENTION !! Ne marche que sur des MCU de hauteur 2 !
+    int16_t *flux_sortie = malloc(MCU_entree->largeur * 64 * sizeof(int16_t));
+    for (int ligne=0; ligne < 8; ligne++)   {
+        for (int i=0; i < MCU_entree->largeur * 8; i++) {
+            flux_sortie[(ligne * MCU_entree->largeur * 8) + i] = (MCU_entree->flux[(MCU_entree->largeur * 8 * 2*ligne) + i]
+                    + MCU_entree->flux[(MCU_entree->largeur * 8 * (2*ligne + 1)) + i]) / (int16_t) 2;
+        }
+    }
+    struct MCU_16 *MCU_sortie = malloc(sizeof(struct MCU_16));
+    MCU_sortie->hauteur = MCU_entree->hauteur / (uint8_t) 2;    // Sera en pratique toujours égal à 1.
+    MCU_sortie->largeur = MCU_sortie->largeur;
+    MCU_sortie->couleur = MCU_entree->couleur;          // Idem, en pratique toujours égal à 1, vu qu'on ne compresse que les images couleurs.
+    MCU_sortie->echant_l = 1;
+    MCU_sortie->echant_h = MCU_entree->echant_h;
+    MCU_sortie->flux = flux_sortie;
+
+    free(MCU_entree);
+    return MCU_sortie;
+}
+
+
+
+void Image_downsampling(struct Image_MCU_16 * Image_entree, int compression_horizontale, int compression_verticale)    {
+    uint64_t nombre_MCUs = Image_entree->largeur * Image_entree->hauteur;
+    for (uint64_t i=0; i < nombre_MCUs; i++)    {
+        if (compression_horizontale)    {
+            assert((Image_entree->MCUs[i]->largeur == 2) && "Il faut que le MCU soit de largeur 2 si on veut compresser horizontalement !\n");
+            Image_entree->MCUs[i] = downsampling_horizontal_2_blocs(Image_entree->MCUs[i]);
+        }
+        if (compression_verticale)    {
+            assert((Image_entree->MCUs[i]->hauteur == 2) && "Il faut que le MCU soit de hauteur 2 si on veut compresser verticalement !\n");
+            Image_entree->MCUs[i] = downsampling_vertical_2_blocs(Image_entree->MCUs[i]);
+        }
+    }
+}
